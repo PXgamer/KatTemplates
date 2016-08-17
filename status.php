c3095f637b47ef3cbf8b52f600564b01edba0d25
ScribeOfGoD <torrents@katmirrors.com>
2016-08-17 06:45:47 -0700

    Header, links, and css
    Changed the size of the header to 85px
    Editited the links so they can be edited later with our URL
    and added different color checkmarks to icons.css

--- status.php
+++ status.php
@@ -2,78 +2,61 @@
 <html>
 	<head>
 	    <meta charset="utf-8">
-		<title>Kickass/KAT/Kickasstorrents - Availability Status + Verified Proxies</title>
+		<title>KAT Status</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="assets/css/screen.css">
 
 		<link rel="shortcut icon" href="favicon.ico"/>
 
-		<meta name="description" content="KickassTorrents site status and official mirrors list">
+		<meta name="description" content="KAT Status">
 		<link rel="manifest" href="manifest.json">
 	</head>
 	<body>
 		<header>
 			<nav>
 				<h1 class="logo">
-					<a href="https://kastatus.com/" style="text-decoration:none">
+					<a href="https://ourdomain.com/" style="text-decoration:none">
 						<img src="assets/img/logo-small.svg" align="left" border=0 width="72">
-						<span>kickass<strong>torrents</strong>
+						<span>Kickass<strong>Torrents</strong>
 						<sub title="100% / 100% / 100%">Site status</sub></span>
 					</a>
 				</h1>
-				<a class="backToSite" href="https://kat.cr/full/">Go to the site</a>
+				<a class="backToSite" href="https:/ourdomain.cr/full/">OUR DOMAIN HERE</a>
 			</nav>
 		</header>
 		<div class="main-content">
 			<div class="content">
 				<section class="b-primaryDomain">
-					<h5>Currently Kickasstorrents primary domain is:</h5>
+					<h5>Currently KickassTorrents primary domain is:</h5>
 					<div class="w-domainLink">
-						<a class="domainLink" href="https://kat.cr/"><i>https://</i>kat.cr</a>
+						<a class="domainLink" href="https://ourdomain.cr/">ourdomain.cr</a>
 					</div>
 				</section>
 				<section>
 									<section class="b-issue">
-							<i class="b-issue-status-icon icon-checkmark-circle"></i>
+							<i class="b-issue-status-icon icon-checkmark-circle-green"></i>
 							<h2>All systems are up and running</h2>
 							<p id="links_text" title="100%" style="color:red">KickassTorrents has been blocked in India. Try changing DNS server to 8.8.8.8</p>
 							<div class="clear"></div>
 						</section>
 									<section class="b-proxy">
-						<p class="grey">Can't access <a href="https://kat.cr/">https://kat.cr</a>? Is it slow or unresponsive? Getting DNS error? Try to use one of our safe mirrors:</p>
+						<p class="grey">Can't access <a href="https://ourdomain.cr/">ourdomain.cr</a>? Is it slow or unresponsive? Getting DNS error? Try to use one of our safe mirrors:</p>
 						<ul>
-							<li><a href="https://kickassto.co/" class="domainLink"><i>https://</i>kickassto.co</a></li>
-							<li><a href="https://kickass.ag/" class="domainLink"><i>https://</i>kickass.ag</a></li>
-							<li><a href="https://kickass.ac/" class="domainLink"><i>https://</i>kickass.ac</a></li>
-							<li><a href="https://thekat.tv/" class="domainLink"><i>https://</i>thekat.tv</a></li>
-							<li><a href="https://thekat.se/" class="domainLink"><i>https://</i>thekat.se</a></li>
-							<li><a href="https://katproxy.is/" class="domainLink"><i>https://</i>katproxy.is</a></li>
-							<li><a href="https://katproxy.cc/" class="domainLink"><i>https://</i>katproxy.cc</a></li>
-							<li><a href="https://lovekat.org/" class="domainLink"><i>https://</i>lovekat.org</a></li>
-							<li><a href="http://lsuzvpko6w6hzpnn.onion/" class="domainLink"><i>http://</i>lsuzvpko6w6hzpnn.onion</a></li>
+							<li><a href="https://Mirror1.co/" class="domainLink">Mirror1.co</i></a></li>
+							<li><a href="https://Mirror2.ag/" class="domainLink">Mirror2.ag</a></li>
+							<li><a href="https://Mirror3.ac/" class="domainLink">Mirror3.ac</a></li>
+							<li><a href="https://Mirror4.tv/" class="domainLink">Mirror4.tv</a></li>
+							<li><a href="https://Mirror5.se/" class="domainLink">Mirror5.se</a></li>
+							<li><a href="https://Mirror6.is/" class="domainLink">Mirror6.is</a></li>
+							<li><a href="https://Mirror7.cc/" class="domainLink">Mirror7.cc</a></li>
+							<li><a href="https://Mirror8.org/" class="domainLink">Mirror8.org</a></li>
+							<li><a href="http://Mirror9.onion/" class="domainLink">Mirror9.onion</a></li>
 						</ul>
 					</section>
 				</section>
 			</div>
 		</div>
 		<footer>
-			<section class="b-push">
-				<h2>Want to stay updated?</h2>
-				<p class="grey">Subscribe to receive push notifications in your browser or follow us in social networks</p>
-				<div id="registerForPushSpan" class="inline-block" style="display: none">
-				  <button id="registerForPushButton" class="btn">RECEIVE PUSH NOTIFICATIONS</button>
-				</div>
-				<div id="not-supported-message" class="inline-block" style="display: none">
-				  <p>Please update your browser to Chrome version 42 or later to receive push notifications!</p>
-				</div>
-				<div id="registered" class="inline-block" style="display: none">
-				  <p class="grey">You are subscribed to receive push notifications!</p>
-				</div>
-				<ul class="sbuttons">
-					<li><a href="https://twitter.com/kickasstorrents?lang=en" class="icon-social-twitter-circular" target="_blank"></a></li>
-					<li><a href="https://www.facebook.com/official.KAT.fanclub" class="icon-social-facebook-circular" target="_blank"></a></li>
-				</ul>
-			</section>
 			<section class="b-request">
 				<h2>Still can't get to the site?</h2>
 				<p class="grey">Tell us about your problem</p>
